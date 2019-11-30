8a08041e1136161c355be52a55946ddd833c88eb
Takayuki Kaburagi <t.kaburagi@me.com>
2019-11-30 21:24:52 +0900

    first commit

---
+++ main.tf
@@ -0,0 +1,53 @@
+# VPC
+resource "aws_vpc" "playground" {
+  cidr_block = var.vpc_cidr
+  instance_tenancy = "default"
+  enable_dns_support = "true"
+  enable_dns_hostnames = "true"
+}
+
+# Subnet
+resource "aws_subnet" "public" {
+  vpc_id = aws_vpc.playground.id
+  count = length(var.availability_zones)
+  cidr_block = var.pubic_subnets_cidr[count.index]
+  availability_zone = var.availability_zones[count.index]
+  tags = {
+    Name = "${var.public_subnet_name}-${count.index}"
+  }
+}
+
+resource "aws_eip" "nat" {
+  vpc = true
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.playground.id
+
+}
+
+# RouteTable
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.playground.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  tags = {
+    Name = "public"
+  }
+}
+
+# SubnetRouteTableAssociation
+resource "aws_route_table_association" "public" {
+  count = length(var.pubic_subnets_cidr)
+  subnet_id = aws_subnet.public.*.id[0]
+  route_table_id = aws_route_table.public.id
+}
+
+# NatGateway
+resource "aws_nat_gateway" "nat" {
+  count = 1
+  subnet_id = aws_subnet.public.*.id[0]
+  allocation_id = aws_eip.nat.id
+}
---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "availability_zones" {
+    type = "list"
+    default = ["ap-northeast-1a"]
+}
+
+variable "vpc_cidr" {
+    default = "10.10.0.0/16"
+}
+
+variable "pubic_subnets_cidr" {
+    type = "list"
+    default = ["10.10.0.0/24", "10.10.1.0/24"]
+}
+
+variable "public_subnet_name" {
+    default = "public"
+}
